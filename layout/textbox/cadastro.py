3afa8e4d0e82d278eace5b80a4cbbf372127e4a9
Veldrin <allangui619@gmail.com>
2025-05-28 11:46:53 -0300

    Update cadastro.py

--- layout/textbox/cadastro.py
+++ layout/textbox/cadastro.py
@@ -59,19 +59,38 @@ def abrir_cadastro():
     entr_curso.pack(pady=5)
     entr_curso.set('Selecione um Curso')
 
+    entr_email=h.CTkEntry(
+        frame,
+        placeholder_text='E-mail'
+    )
+    entr_email.pack(pady=5)
+
+    generos=['Masculino', 'Feminino', 'Não quero informar']
+    entr_genero=h.CTkOptionMenu(
+        frame,
+        values=generos
+    )
+    entr_genero.pack(pady=5)
+    entr_genero.set('Selecione seu Gênero')
+
     def limpar():
         entr_nome.delete(0,'end')
         entr_idade.delete(0,'end')
         entr_curso.set('Selecione um Curso')
+        entr_email.delete(0,'end')
+        entr_genero.set('Selecione seu Gênero')
 
     def enviar():
         nome=entr_nome.get()
         idade=entr_idade.get()
         curso=entr_curso.get()
+        email=entr_email.get()
+        genero=entr_genero.get()
 
-        if nome and idade and curso:
-            messagebox.showinfo('Cadastro Realizado', f'Nome: {nome}\nIdade: {idade}\nE-mail: {curso}')
-            print(f'Nome: {nome}, Idade: {idade}, E-mail: {curso}')
+
+        if nome and idade and curso and email and genero:
+            messagebox.showinfo('Cadastro Realizado', f'Nome: {nome}\nIdade: {idade}\nCurso: {curso}\nE-mail: {email}\nGênero: {genero}')
+            print(f'Nome: {nome}, Idade: {idade}, Curso: {curso} E-mail: {email} Gênero: {genero}')
         else:
             messagebox.showwarning('Campos Incompletos', 'Por gentileza, preencha todos os campos.')
 
