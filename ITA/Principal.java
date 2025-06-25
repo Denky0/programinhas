475839ffb198706bab3221613a60d26504cf3e44
Veldrin <allangui619@gmail.com>
2025-06-25 00:38:19 -0300

    Update Principal.java

--- ITA/Principal.java
+++ ITA/Principal.java
@@ -1,12 +1,3 @@
 public class Principal {
-    public static void main(String[] args) {
-        Paciente paciente1 = new Paciente(50, 1.60);
-        Paciente paciente2 = new Paciente (80, 1.75);
-        Paciente paciente3 = new Paciente (110, 1.70);
-
-
-        System.out.println("Paciente 1: IMC = " + paciente1.calcularIMC()+ " -> " + paciente1.diagnostico());
-        System.out.println("Paciente 2: IMC = " + paciente2.calcularIMC()+ " -> " + paciente2.diagnostico());
-        System.out.println("Paciente 3: IMC = " + paciente3.calcularIMC()+ " -> " + paciente3.diagnostico());
-    }
+    
 }
