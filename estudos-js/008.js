8e999bdd3dce671164f8a92aa1bdfd90ed10e4ec
Veldrin <allangui619@gmail.com>
2025-08-13 20:28:41 -0300

    Update 008.js

--- estudos-js/008.js
+++ estudos-js/008.js
@@ -1,5 +1,11 @@
-var valores = [8, 1, 7, 4, 2, 9]
+var valores = [8, 1, 7, 4, 2, 9] // Vetores em JavaScript
 
+// Método For
 for (var pos = 0; pos < valores.length; pos++ ) {
+    console.log(`Posição ${pos} | Valor ${valores[pos]}`)
+}
+
+// Método For | In
+for (var pos in valores) {
     console.log(`Posição ${pos} | Valor ${valores[pos]}`)
 }
